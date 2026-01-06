17927dbe9427312d3720a79f04856efb56ce9830
Gannar <gannarmeriem@gmail.com>
2026-01-06 16:36:33 +0100

    Add ShoppingCartItem class with price calculation

---
+++ ShoppingCartItem.js
@@ -0,0 +1,10 @@
+class ShoppingCartItem {
+  constructor(product, quantity) {
+    this.product = product;
+    this.quantity = quantity;
+  }
+
+  getTotalPrice() {
+    return this.product.price * this.quantity;
+  }
+}
