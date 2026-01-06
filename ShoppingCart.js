d12937043bc48ceaa9d9a931b55aa8bc315252e7
Gannar <gannarmeriem@gmail.com>
2026-01-06 16:40:02 +0100

    Refactor ShoppingCart methods for clarity and efficiency

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,49 +1,39 @@
-class ShoppingCart {
+import { ShoppingCartItem } from "./ShoppingCartItem.js";
+
+export class ShoppingCart {
   constructor() {
     this.items = [];
   }
 
-  // Add item to cart
   addItem(product, quantity) {
-    const existingItem = this.items.find(
-      item => item.product.id === product.id
-    );
+    const item = this.items.find(i => i.product.id === product.id);
 
-    if (existingItem) {
-      existingItem.quantity += quantity;
+    if (item) {
+      item.quantity += quantity;
     } else {
       this.items.push(new ShoppingCartItem(product, quantity));
     }
   }
 
-  // Remove item from cart
   removeItem(productId) {
     this.items = this.items.filter(
       item => item.product.id !== productId
     );
   }
 
-  // Get total price of all items
   getTotal() {
     return this.items.reduce(
-      (total, item) => total + item.getTotalPrice(),
+      (sum, item) => sum + item.getTotalPrice(),
       0
     );
   }
 
-  // Display cart items
   displayCart() {
-    if (this.items.length === 0) {
-      console.log("Cart is empty");
-      return;
-    }
-
     this.items.forEach(item => {
       console.log(
         `${item.product.name} x ${item.quantity} = $${item.getTotalPrice()}`
       );
     });
-
     console.log(`Total: $${this.getTotal()}`);
   }
 }
